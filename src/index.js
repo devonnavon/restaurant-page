cf4f0aba4897a9101de9ec7713e318f60d05b7af
Devon Navon <devon.navon@gmail.com>
2020-05-27 15:48:45 -0700

    we got tabs workin, all that, commit it

--- src/index.js
+++ src/index.js
@@ -2,30 +2,23 @@ import {loadPage} from './loadPage'
 
 loadPage('home')
 
-const homeNav = document.getElementById('home-nav')
-const locationNav = document.getElementById('location-nav')
-const menuNav = document.getElementById('menu-nav')
+const homeNav = document.getElementById('home-nav').querySelector('input')
+const locationNav = document.getElementById('location-nav').querySelector('input')
+const menuNav = document.getElementById('menu-nav').querySelector('input')
 
 homeNav.addEventListener('click', e=>{
-    e.preventDefault()
+    // e.preventDefault()
     loadPage('home');
 })
 
 locationNav.addEventListener('click', e=>{
-    e.preventDefault()
+    // e.preventDefault()
     loadPage('location');
 })
 
 menuNav.addEventListener('click', e=>{
-    e.preventDefault()
+    // e.preventDefault()
     loadPage('menu');
 })
 
 
-// locationNav.addEventListener('click', loadPage('location'))
-// menuNav.addEventListener('click', loadPage('menu'))
-
-// const homeClickEvent = (e, option) => {
-//     e.preventDefault()
-//     loadPage
-// }
--- src/loadPage.js
+++ src/loadPage.js
@@ -8,6 +8,8 @@ const content = document.getElementById('content');
 const loadPage = (option) => {
     clearContent()
     if(option === 'home') loadHome();
+    if(option === 'location') loadLocation();
+    if(option === 'menu') loadMenu();
 
 }
 
@@ -35,6 +37,59 @@ const loadHome = () => {
     home.appendChild(mainDescription);
     content.appendChild(home)
 }
+
+const loadLocation = () => {
+    const location = createElement('location');
+    
+    const image = createElement('location-image');
+    const header = createElement('location-header');
+    const mainDescription = createElement('location-description');
+    
+    addImage(image,'img/whereweat.jpg', 'the place to be');
+    addHeading(header, 'The Pond')
+
+    const text = 'We are located in the hills. It is quite nice up in the hills. Lots of greens.'
+
+    addParagraph(mainDescription,text)
+
+    location.appendChild(header);
+    location.appendChild(mainDescription);
+    location.appendChild(image);
+    content.appendChild(location);
+}
+
+const loadMenu = () => {
+    
+    const menu = createElement('menu');
+    
+    const item1 = createElement('sushi')
+    const item2 = createElement('burrito')
+    const item3 = createElement('fish')
+    const item4 = createElement('veggies')
+
+    addHeading(item1, 'sushi')
+    addParagraph(item1, 'we got sushi, dog.')
+
+
+    addHeading(item2, 'burrito')
+    addParagraph(item2, 'we\'re makin burritos out here' )
+
+
+    addHeading(item3, 'fish')
+    addParagraph(item3, 'we got fish, dog.')
+    
+    addHeading(item4, 'veggies')
+    addParagraph(item4, 'veggies for you my lord...')
+    
+    menu.appendChild(item1);
+    menu.appendChild(item2);
+    menu.appendChild(item3);
+    menu.appendChild(item4);
+    
+    content.appendChild(menu);
+}
+
+
 
 
 
